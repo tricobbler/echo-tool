0ad9604ca9abb3c81e0faa87253bf541b27e927a
duyan <duyan@rp-pet.cn>
2020-12-22 17:00:47 +0800

    增加echo框架参数验证插件

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/tricobbler/echoTool
 go 1.15
 
 require (
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/maybgit/glog v0.0.1
 	github.com/spf13/cast v1.3.1
---
+++ validate/validator.go
@@ -0,0 +1,67 @@
+package validate
+
+import (
+	zhongwen "github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	"reflect"
+	"strings"
+)
+
+var (
+	V     *validator.Validate
+	trans ut.Translator
+)
+
+type (
+	validateErrList []string
+	customValidator struct {
+		validator *validator.Validate
+	}
+)
+
+func (cv *customValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+func NewCustomValidator() *customValidator {
+	return &customValidator{validator: V}
+}
+
+func init() {
+	zh := zhongwen.New()
+	uni := ut.New(zh, zh)
+	trans, _ = uni.GetTranslator("zh")
+
+	V = validator.New()
+	V.RegisterTagNameFunc(func(field reflect.StructField) string {
+		label := field.Tag.Get("label")
+		if label == "" {
+			return field.Name
+		}
+		return label
+	})
+	zh_translations.RegisterDefaultTranslations(V, trans)
+}
+
+func Translate(errs validator.ValidationErrors) validateErrList {
+	var errList validateErrList
+	for _, e := range errs {
+		// can translate each error one at a time.
+		errList = append(errList, e.Translate(trans))
+	}
+	return errList
+}
+
+func (v validateErrList) One() string {
+	if len(v) == 0 {
+		return ""
+	}
+
+	return v[0]
+}
+
+func (v validateErrList) All() string {
+	return strings.Join(v, "|")
+}
